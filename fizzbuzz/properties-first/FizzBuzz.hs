85bbbd6c4d4ee7714d09624519c8f0bd2e0aca27
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-11-21 17:16:57 +0100

    Fizzbuzz properties first: force the number of lines to match the number asked

--- fizzbuzz/properties-first/FizzBuzz.hs
+++ fizzbuzz/properties-first/FizzBuzz.hs
@@ -6,5 +6,6 @@ main = error "NE"
 -- FizzBuzz.
 --
 -- prop> all (flip elem ['F', 'i', 'z', 'B', 'u', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '\n']) (fizzbuzz x)
-fizzbuzz :: Integer -> String
-fizzbuzz _ = "F"
+-- prop> x >= 0 ==> length (lines (fizzbuzz x)) == x
+fizzbuzz :: Int -> String
+fizzbuzz n = replicate n  '\n'
