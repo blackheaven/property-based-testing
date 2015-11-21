81d801791399fb375d1d39e1f67ab1dc356463cf
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-11-21 17:19:46 +0100

    Fizzbuzz properties first: force lines to don't be empty

--- fizzbuzz/properties-first/FizzBuzz.hs
+++ fizzbuzz/properties-first/FizzBuzz.hs
@@ -7,5 +7,6 @@ main = error "NE"
 --
 -- prop> all (flip elem ['F', 'i', 'z', 'B', 'u', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '\n']) (fizzbuzz x)
 -- prop> x >= 0 ==> length (lines (fizzbuzz x)) == x
+-- prop> all (not . null) (lines $ fizzbuzz x)
 fizzbuzz :: Int -> String
-fizzbuzz n = replicate n  '\n'
+fizzbuzz n = concat $ replicate n  "F\n"
