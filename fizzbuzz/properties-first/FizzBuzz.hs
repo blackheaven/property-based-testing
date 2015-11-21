c111e315bb5ea9a9c44fde1e4efef284b13f744b
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-11-21 17:47:24 +0100

    Fizzbuzz properties first: force lines to be either a number or Fizz or Buzz or FizzBuzz

--- fizzbuzz/properties-first/FizzBuzz.hs
+++ fizzbuzz/properties-first/FizzBuzz.hs
@@ -1,4 +1,5 @@
 module FizzBuzz where
+import Data.Char(isDigit)
 
 main = error "NE"
 
@@ -8,5 +9,6 @@ main = error "NE"
 -- prop> all (flip elem ['F', 'i', 'z', 'B', 'u', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '\n']) (fizzbuzz x)
 -- prop> x >= 0 ==> length (lines (fizzbuzz x)) == x
 -- prop> all (not . null) (lines $ fizzbuzz x)
+-- prop> all (\l -> all isDigit l || elem l ["Fizz", "Buzz", "FizzBuzz"]) (lines $ fizzbuzz x)
 fizzbuzz :: Int -> String
-fizzbuzz n = concat $ replicate n  "F\n"
+fizzbuzz n = unlines $ map show [1..n]
