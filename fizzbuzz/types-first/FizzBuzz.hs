fa6ef6978c5c84ac4d3cc23a5ebaccd62b25faaa
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-11-22 19:52:54 +0100

    Fizzbuzz types first: refactor tests

--- fizzbuzz/types-first/FizzBuzz.hs
+++ fizzbuzz/types-first/FizzBuzz.hs
@@ -8,8 +8,8 @@ data FizzBuzzResult = Number { extractNumber :: StrictlyPositive Int }
 -- |
 -- FizzBuzz.
 --
--- prop> x > 0 ==> Just x == fmap (length . fizzbuzz) (mkStrictlyPositive x)
--- prop> x > 0 ==> Just True == fmap ((all (\(i, n) -> not (isNumber n) || i == getNumber (extractNumber n))) . zip [1..n] . fizzbuzz) (mkStrictlyPositive x)
+-- prop> assert x $ (== x) . length
+-- prop> assert x $  (all (\(i, n) -> not (isNumber n) || i == getNumber (extractNumber n))) . zip [1..n]
 fizzbuzz :: StrictlyPositive Int -> [FizzBuzzResult]
 fizzbuzz (StrictlyPositive n) = map Number $ mapMaybe mkStrictlyPositive [1..n]
 
@@ -21,3 +21,8 @@ mkStrictlyPositive n = if n > 0 then Just (StrictlyPositive n) else Nothing
 
 isNumber :: FizzBuzzResult -> Bool
 isNumber = const True
+
+assert :: Int -> ([FizzBuzzResult] -> Bool) -> Bool
+assert n p
+  | n > 0     = Just True == fmap (p . fizzbuzz) (mkStrictlyPositive n)
+  | otherwise = True
