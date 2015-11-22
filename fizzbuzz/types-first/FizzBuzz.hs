c95101d1e30134139373c6f74d5ad4659d1cf8c8
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-11-22 20:35:28 +0100

    Fizzbuzz types first: refactor divisibility

--- fizzbuzz/types-first/FizzBuzz.hs
+++ fizzbuzz/types-first/FizzBuzz.hs
@@ -18,10 +18,10 @@ fizzbuzz (StrictlyPositive n) = map fizzbuzzForIndex $ mapMaybe mkStrictlyPositi
 -- |
 -- FizzBuzz for a given index.
 --
--- prop> assertIndex x $ \n -> mod x 3 /= 0 || not (isNumber n)
--- prop> assertIndex x $ \n -> mod x 5 /= 0 || not (isNumber n)
+-- prop> assertIndex x $ \n -> notDivisibleDy x 3 || not (isNumber n)
+-- prop> assertIndex x $ \n -> notDivisibleDy x 5 || not (isNumber n)
 fizzbuzzForIndex :: StrictlyPositive Int -> FizzBuzzResult
-fizzbuzzForIndex xe@(StrictlyPositive x) = if mod x 3 == 0 || mod x 5 == 0 then Other else Number xe
+fizzbuzzForIndex xe@(StrictlyPositive x) = if divisibleBy x 3 || divisibleBy x 5 then Other else Number xe
 
 -- Helpers
 newtype StrictlyPositive a = StrictlyPositive { getNumber :: a } deriving Eq
@@ -46,3 +46,9 @@ assertIndex :: Int -> (FizzBuzzResult -> Bool) -> Bool
 assertIndex n p
   | n > 0     = Just True == fmap (p . fizzbuzzForIndex) (mkStrictlyPositive n)
   | otherwise = True
+
+divisibleBy :: Integral a => a -> a -> Bool
+divisibleBy a b = mod a b == 0
+
+notDivisibleDy :: Integral a => a -> a -> Bool
+notDivisibleDy a b = not $ divisibleBy a b
